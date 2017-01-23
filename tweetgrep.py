bf8cc890229521f64ab2dafe487ece6c68ab216d
Doza <mcardosa@gmail.com>
2017-01-22 22:44:50 -0500

    Added -b option and simplified the logging a bit so it goes to stderr instead of a file.

--- tweetgrep.py
+++ tweetgrep.py
@@ -69,7 +69,7 @@ def fetch_all_tweets(screen_name):
     
     #keep grabbing tweets until there are no tweets left to grab
     while len(new_tweets) > 0:
-        logger.debug("getting tweets before %s" % (oldest))
+        logging.debug("getting tweets before %s" % (oldest))
         
         #all subsiquent requests use the max_id param to prevent duplicates
         new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
@@ -80,7 +80,7 @@ def fetch_all_tweets(screen_name):
         #update the id of the oldest tweet less one
         oldest = all_tweets[-1].id - 1
         
-        logger.debug("...%s tweets downloaded so far" % (len(all_tweets)))
+        logging.debug("...%s tweets downloaded so far" % (len(all_tweets)))
     
     #transform the tweepy tweets into a 2D array that will populate the csv 
     results = [[screen_name, tweet.id_str, tweet.created_at, tweet.text, "https://twitter.com/%s/status/%s" % (screen_name, tweet.id_str)] for tweet in all_tweets]
@@ -116,6 +116,9 @@ def main():
     parser.add_argument("-i", "--ignore-case", help="Ignore case when searching",
         action="store_true", dest="ignore_case",
         default=False)
+    parser.add_argument("-b", "--brief-output", help="Only show the contents of the tweets without extra information (date and link)",
+        action="store_true", dest="brief_output",
+        default=False)
     parser.add_argument("-r", "--regex", help="Search using a regex instead of a simple string",
         action="store_true", dest="use_regex",
         default=False)
@@ -126,61 +129,54 @@ def main():
 
     args = parser.parse_args()
 
-    global logger
-    logger = logging.getLogger(__name__)
-    logger.setLevel(level=args.log_level)
-    handler = logging.FileHandler('tweetgrep.log')
-    handler.setLevel(args.log_level)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', level=args.log_level)
 
-    logger.debug("Twitter name: %s" % args.twitter_name)
+    logging.debug("Twitter name: %s" % args.twitter_name)
     twitter_name = args.twitter_name
-    logger.debug("Search term: %s" % args.search_string)
+    logging.debug("Search term: %s" % args.search_string)
     search_string = args.search_string
 
     cache_name = "%s_cache.dat" % twitter_name
-    logger.debug("Cache name: %s" % cache_name)
+    logging.debug("Cache name: %s" % cache_name)
 
     try:
         cache_modified_time = os.path.getmtime(cache_name)
         cache_exists = True
-        logger.debug("Found existing cache file")
+        logging.debug("Found existing cache file")
         cache_modified_date = datetime.fromtimestamp(cache_modified_time)
-        logger.debug("Cache modified date: %s" % cache_modified_date)
+        logging.debug("Cache modified date: %s" % cache_modified_date)
         day_diff = datetime.now() - cache_modified_date
-        logger.debug("Cache file is %s days old" % day_diff.days)
+        logging.debug("Cache file is %s days old" % day_diff.days)
     except:
         cache_exists = False
-        logger.debug("Did not find existing cache file")
+        logging.debug("Did not find existing cache file")
         create_cache = True
     
     
     if cache_exists and day_diff.days >= 1:
         #If the cache is older than 1 day we try to refresh it
-        logger.debug("Attempting to delete old cache file")
+        logging.debug("Attempting to delete old cache file")
         try:
             os.remove(cache_name)
-            logger.debug("Cache file successfully deleted")
+            logging.debug("Cache file successfully deleted")
             create_cache = True
         except OSError:
-            logger.warn("Could not remove old cache file")
-            logger.warn("Results could be stale. You should manually remove %s" % cache_name)
+            logging.warn("Could not remove old cache file")
+            logging.warn("Results could be stale. You should manually remove %s" % cache_name)
             create_cache = False
     elif cache_exists:
-        logger.debug("Cache file is relatively new and will not be removed")
+        logging.debug("Cache file is relatively new and will be used for this search ")
         create_cache = False
     else:
         create_cache = True
-        logger.debug("Need to create a new cache file")
+        logging.debug("Need to create a new cache file")
 
     if create_cache:
         user_tweets = fetch_all_tweets(twitter_name)
 
         #write the csv  
         with open(cache_name, 'wt') as f:
-            logger.debug("Creating new cache file")
+            logging.debug("Creating new cache file")
             writer = csv.writer(f)
             writer.writerow(["screen_name","id","created_at","text","status_link"])
             writer.writerows(user_tweets)
@@ -200,12 +196,17 @@ def main():
             else:
                 tweet = row[3]
 
+            if args.brief_output:
+                output = "%s" % row[3]
+            else:
+                output = "%s\t%s\n%s" % (row[2], row[3], row[4])
+
             if not args.use_regex and search_string in tweet:
                 hit_counter += 1
-                print("%s\t%s\n%s" % (row[2], row[3], row[4]))
+                print(output)
             elif args.use_regex and regex.search(tweet):
                 hit_counter += 1
-                print("%s\t%s\n%s" % (row[2], row[3], row[4]))
+                print(output)
             else:
                 pass
 
